f577bb7366e2c8b8953c38fb5074208b775dfb78
Ahmed Raza -ArabicWeb <KMCAR516@dawateislami.lan>
2018-05-04 12:52:08 +0500

    Real stuff integrated

--- js/script.js
+++ js/script.js
@@ -246,3 +246,50 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+	$(document).ready(function () {
+			$('.tooltipped').tooltip();
+		});
+		$(document).ready(function () {
+			$('.carousel').carousel();
+		});
+		$(document).ready(function () {
+			$('.parallax').parallax();
+		});
+		$(document).ready(function () {
+			$('.scrollspy').scrollSpy();
+		});
+
+		document.addEventListener('DOMContentLoaded', () => {
+			// This is the bare minimum JavaScript. You can opt to pass no arguments to setup.
+			const player = new Plyr('#player');
+
+			// Bind event listener
+			function on(selector, type, callback) {
+				document.querySelector(selector).addEventListener(type, callback, false);
+			}
+
+			// Play
+			on('.js-play', 'click', () => {
+				player.play();
+			});
+
+			// Pause
+			on('.js-pause', 'click', () => {
+				player.pause();
+			});
+
+			// Stop
+			on('.js-stop', 'click', () => {
+				player.stop();
+			});
+
+			// Rewind
+			on('.js-rewind', 'click', () => {
+				player.rewind();
+			});
+
+			// Forward
+			on('.js-forward', 'click', () => {
+				player.forward();
+			});
+		});
